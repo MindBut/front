ce3902c31f3ce8c8fb7fbb20a917837170a06442
Jimin Yoo <jmy0819_@naver.com>
2023-12-22 02:46:37 +0900

    feat: Calendar

--- MindBut/screens/MoodRecord.jsx
+++ MindBut/screens/MoodRecord.jsx
@@ -10,22 +10,81 @@ import {
 import axios from 'axios';
 import { Colors, Device, Fonts } from '../components/common/styles';
 import { useNavigation } from '@react-navigation/native';
-import Calender from '../components/moodrecord/Calender';
 import MoodTrackingElement from '../components/moodrecord/MoodTrackingElement';
 import CheckInElement from '../components/moodrecord/CheckInElement';
 import ListIcon from '../assets/icons/list.svg'; 
 import BubbleIcon from '../assets/icons/bubble.svg';
+import { Calendar } from 'react-native-calendars';
+import { useEffect, useState } from 'react';
 
 
 export default MoodRecord = () => {
+
+  // Navigation
   const navigation = useNavigation();
 
+  // States
+  const [selectedDate, setSelectedDate] = useState();
+  const [markedDates, setMarkedDates] = useState();
+  const data = ['2023-12-18', '2023-12-19'];
+
+  const getDataFromDate = async (dateString) => {
+    // console.log(dateString);
+  }
+
+  // Effects
+  useEffect(() => {
+    axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    ).then(
+      () => {
+        const dates = data.reduce(
+          (c, v) => 
+            Object.assign(c, {
+              [v]: { selected: false, dots: [happy, angry] }
+            }),
+          {}
+        );
+        setMarkedDates(dates);
+        console.log(dates);
+      }
+    )
+  }, []);
+
+  
+  // Calender
+  const happy = {key: 'happy', color: '#42def3'};
+  const embarrassed = {key: 'embarrassed', color: '#44f964', selectedDotColor: 'blue'};
+  const angry = {key: 'angry', color: '#7e58f7'};
+  const anxious = {key: 'anxious', color: '#ffdf46'};
+  const hurt = {key: 'hurt', color: '#ffc146'};
+  const sad = {key: 'sad', color: '#ff5a5a'};
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
-      <View style={styles.calenderArea}>
-        <Calender />
+      <View style={styles.calendarArea}>
+        <Calendar
+          theme={{
+            selectedDayBackgroundColor: Colors.tertiary,
+            selectedDayTextColor: Colors.grayText,
+            todayTextColor: Colors.primary,
+            textMonthFontFamily: Fonts.header,
+            textMonthFontSize: 18,
+            textMonthFontWeight: 'bold',
+            textDayFontFamily: Fonts.body,
+            arrowColor: Colors.lightGrayUI,
+          }}
+          onDayPress={(day) => setSelectedDate(day.dateString)}
+          markingType={'multi-dot'}
+          markedDates={{
+            ...markedDates,
+            [selectedDate]: {
+              selected: true,
+              dots: markedDates[selectedDate]?.dots,
+            }
+          }}
+        />
       </View>
       <ScrollView style={styles.recordsArea}>
         <Text style={{fontSize: 28, fontFamily: Fonts.header}}>무드 트래킹</Text>
@@ -64,17 +123,18 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.trueWhite,
     alignItems: 'center',
   },
-  calenderArea: {
+  calendarArea: {
     width: Device.fullLayoutWidth,
-    height: '40%',
-    paddingHorizontal: 20,
+    height: '47%',
     
-    borderWidth: 3,
+    // borderWidth: 3,
     borderColor: 'red'
   },
   recordsArea: {
     // borderWidth: 1,
-    borderColor: 'blue',
+    borderTopWidth: 1,
+    paddingTop: 16,
+    borderColor: Colors.lightGray,
     width: Device.fullLayoutWidth,
   },
   navigatorArea: {
@@ -90,11 +150,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     borderTopWidth: 1,
-    borderColor: Colors.lightGrayUI,
+    borderColor: Colors.lightGray,
   },
   navigatorText: {
     marginHorizontal: 10,
-    fontFamily: Fonts.header,
+    fontFamily: Fonts.body,
     fontSize: 18,
     color: Colors.lightGrayUI,
   }
